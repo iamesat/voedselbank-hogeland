2bedb47dc1dc053c8aff6426f3bae7665a1880ea
Annet <aeyavas@outlook.com>
2020-01-16 11:14:29 +0100

    Klantenpagina en toets bijgwerkt
    Klantenpagina en toets bijgwerkt

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+
+
+$('#buttonstap2').click(function() {
+    $('#stap2').css({
+        'background-image': 'none',
+        'opacity': '1',
+        'pointer-events': 'all',
+    });
+});
+
+$('#buttonstap3').click(function() {
+    $('#stap3').css({
+        'background-image': 'none',
+        'opacity': '1',
+        'pointer-events': 'all',
+    });
+});
