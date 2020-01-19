53dc07a1f4c15581869b2627b71bb7bc53392ac7
Lydwe van Westen <lydwe@intuitivedesign.nl>
2020-01-19 14:16:46 +0100

    Werkend maken criteriatoets

--- js/javascript.js
+++ js/javascript.js
@@ -1,11 +1,16 @@
 $('#buttonstap2').click(function() {
 	$('#stap2').removeClass('hide');
+
+	var numberOfPersons = $('#personenID').val();
+	var total = 135 + numberOfPersons * 90;
+
+	$('#total').text(total);
 });
 
 $('#buttonstap3').click(function() {
 	$('#stap3').removeClass('hide');
 });
 
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
+$(document).ready(function() {
+	$('[data-toggle="popover"]').popover();
 });
