d1fe3a0ba252a4bb889246df0ffc1009236756d3
Lydwe van Westen <lydwe@intuitivedesign.nl>
2020-01-19 14:31:13 +0100

    Make shit happen criteriatoets

--- js/javascript.js
+++ js/javascript.js
@@ -9,6 +9,19 @@ $('#buttonstap2').click(function() {
 
 $('#buttonstap3').click(function() {
 	$('#stap3').removeClass('hide');
+
+	var outcomeTitle;
+	var outcomeText;
+
+	if($('#customRadioInline1').prop("checked") === true) {
+		outcomeTitle = "Helaas, u komt niet in aanmerking voor een voedselpakket van de voedselbank";
+		outcomeText = "Twijfelt u aan deze uitslag, dan kunt u contact opnemen met een van onze externe instanties.";
+	} else if($('#customRadioInline2').prop("checked") === true) {
+		outcomeTitle = "U komt in aanmerking voor een voedselpakket van de voedselbank!";
+		outcomeText = "Afhankelijk van uw situatie kunt u contact zoeken met een externe instanties om de aanmelding te voltooien.";
+	}
+	$('#outcomeTitle').text(outcomeTitle);
+	$('#outcomeText').text(outcomeText);
 });
 
 $(document).ready(function() {
